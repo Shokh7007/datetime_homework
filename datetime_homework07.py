bf5589d3eb10175f7de4fd6ef4c52899f80cf05a
Shokh7007 <shoxruxbeknafasov9@gmail.com>
2023-05-26 15:55:26 +0500

    7

--- datetime_homework07.py
+++ datetime_homework07.py
@@ -1,2 +1,6 @@
 #Task 7:
 #Write a Python program that adds 5 days to the current date and displays the resulting date.
+import datetime
+today=datetime.date.today()
+date=datetime.date(year=today.year,month=today.month,day=today.day+5)
+print(date)
